e7158f2c9b5b76f53b939fd922ae6b708e3130e2
oM <119602009+omcodedthis@users.noreply.github.com>
2023-02-24 22:46:36 +0800

    HTML, CSS & JavaScript

---
+++ Week 8/Problem Set 8/homepage/checkclicks.js
@@ -0,0 +1,21 @@
+// this ensures the code runs only when the page is fully loaded for the user.
+document.addEventListener('DOMContentLoaded', function() {
+
+    // assigns the button with the class .correct to correct, and if it is clicked, its bg-color is
+    // set to green and "Correct!" appears below the options.
+    let correct = document.querySelector('.correct');
+    correct.addEventListener('click', function() {
+        correct.style.backgroundColor = 'green';
+        document.querySelector('#feedback1').innerHTML = 'Correct!';
+    });
+
+    // assigns the buttons with the class .incorrect to wrong_answers, which is an array, and if they
+    // are clicked, its bg-color is set to red and "Incorrect" appears below the options.
+    let wrong_answers = document.querySelectorAll('.incorrect');
+    for (let i = 0; i < wrong_answers.length; i++){
+        wrong_answers[i].addEventListener('click', function() {
+            wrong_answers[i].style.backgroundColor = 'red';
+            document.querySelector('#feedback1').innerHTML = 'Incorrect';
+        });
+    }
+});
---
+++ Week 8/Problem Set 8/homepage/clickme.js
@@ -0,0 +1,5 @@
+function alertme(){
+    alert("Hello, thanks for clicking me!");
+}
+
+function 
